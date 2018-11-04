d7919fae3f056d6baea6c03bf1bf36bdaa39f7fc
mike_ls <elzzzad@gmail.com>
2018-11-04 13:58:57 +0200

    fail mssg

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$('document').ready(()=>{
+
+
+// $('.menuWr').on('click', ()=> {
+
+// $('.menuBlock').css('display', 'flex');
+
+// });
+
+// $('.close').on('click', ()=>{
+
+// $( ".menuBlock" ).animate({ opacity: 0}, 100, ()=>{
+// $('.menuBlock').css({display: 'none', opacity: '1'});	
+// } );
+
+// $('.menuBlock').css('display', 'none');
+// console.log("ok");
+
+// });
+
+// $('.close').hover( ()=> {
+
+// $('.menuBlock').css('background-color', 'rgba(18, 22, 49, .6)');
+// $('.menuWr').css('display', 'none');
+
+// }, ()=> {
+
+// $('.menuBlock').css('background-color', 'rgba(18, 22, 49, 1)');
+// $('.menuWr').css('display', 'block');
+
+// });
+
+
+
+
+
+});
+
+
